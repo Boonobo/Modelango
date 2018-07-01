d9bbdd34738e2af056a5e1f9c2e58ea5a2c591de
Kamil Kacprzak <kamilkacprzak2@gmail.com>
2018-07-01 15:38:34 +0200

    Initial commit

---
+++ app/src/main/java/kamilkacprzak16185927/modelango/MainActivity.java
@@ -0,0 +1,13 @@
+package kamilkacprzak16185927.modelango;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
