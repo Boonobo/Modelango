b761a99650cadf680081621e88aadd5ce71fd8f2
Kamil Kacprzak <kamilkacprzak2@gmail.com>
2018-07-02 11:10:45 +0200

    Camera is working properly

--- app/src/main/java/kamilkacprzak16185927/modelango/CreateModelActivity.java
+++ app/src/main/java/kamilkacprzak16185927/modelango/CreateModelActivity.java
@@ -1,27 +1,26 @@
 package kamilkacprzak16185927.modelango;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.os.AsyncTask;
-import android.os.Handler;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.SurfaceHolder;
-import android.view.ViewGroup;
-import android.widget.ProgressBar;
+import android.util.Log;
+import android.widget.FrameLayout;
 
 public class CreateModelActivity extends AppCompatActivity {
     private Camera mCamera;
-    private Handler mCustomHandler = new Handler();
+    private Preview mPreview;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_model);
+        mPreview = new Preview(this.getApplicationContext(),mCamera);
+        new CameraOpenTask().execute();
     }
 
     private class CameraOpenTask extends AsyncTask<Void, Void, Camera> {
@@ -34,16 +33,19 @@ public class CreateModelActivity extends AppCompatActivity {
         protected Camera doInBackground(Void... params) {
             if (ContextCompat.checkSelfPermission(CreateModelActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
                 return safeCameraOpen(0);
+            }else {
+                Log.d("Lack of permission","Camera is not open due lack of permission" );
+                return null;
             }
-            return null;
         }
 
         @Override
         protected void onPostExecute(Camera camera) {
             mCamera = camera;
             if(mCamera != null){
-
-                //Set this camera instance in camera preview
+                mPreview.setCamera(mCamera);
+                FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+                preview.addView(mPreview);
             }
 
 
@@ -68,16 +70,10 @@ public class CreateModelActivity extends AppCompatActivity {
     }
 
     private void releaseCameraAndPreview() {
+        mPreview.setCamera(null);
         if (mCamera != null) {
             mCamera.release();
             mCamera = null;
         }
     }
-
-//    private class Preview extends ViewGroup implements SurfaceHolder.Callback{
-//
-//        Preview(Context context){
-//            super(context);
-//        }
-//    }
 }
---
+++ app/src/main/java/kamilkacprzak16185927/modelango/Preview.java
@@ -0,0 +1,145 @@
+package kamilkacprzak16185927.modelango;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.Display;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.WindowManager;
+
+import java.io.IOException;
+import java.util.List;
+
+import static android.content.Context.WINDOW_SERVICE;
+
+public class Preview extends SurfaceView implements SurfaceHolder.Callback{
+
+    private SurfaceHolder mSurfaceHolder;
+    private Camera mCamera;
+    private List<Camera.Size> mSupportedPreviewSizes;
+    private Context mContext;
+
+    Preview(Context context, Camera camera){
+        super(context);
+        this.mContext = context;
+        mSurfaceHolder = getHolder();
+        mSurfaceHolder.addCallback(this);
+        mSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        setCamera(camera);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            mCamera.setPreviewDisplay(holder);
+            mCamera.startPreview();
+        } catch (IOException e) {
+            Log.d(VIEW_LOG_TAG,"Error setting camera preview: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        if (mSurfaceHolder.getSurface() == null){
+            // preview surface does not exist
+            return;
+        }
+        try {
+            mCamera.stopPreview();
+        } catch (Exception e){
+            // ignore: tried to stop a non-existent preview
+        }
+
+        Camera.Parameters parameters = mCamera.getParameters();
+        Display display = ((WindowManager)mContext.getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
+
+        if(display.getRotation() == Surface.ROTATION_0) {
+            parameters.setPreviewSize(height, width);
+            mCamera.setDisplayOrientation(90);
+        }
+
+        if(display.getRotation() == Surface.ROTATION_90) {
+            parameters.setPreviewSize(width, height);
+        }
+
+        if(display.getRotation() == Surface.ROTATION_180) {
+            parameters.setPreviewSize(height, width);
+        }
+
+        if(display.getRotation() == Surface.ROTATION_270) {
+            parameters.setPreviewSize(width, height);
+            mCamera.setDisplayOrientation(180);
+        }
+
+        // Important: Call startPreview() to start updating the preview surface.
+        // Preview must be started before you can take a picture.
+        try {
+            mCamera.setPreviewDisplay(mSurfaceHolder);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mCamera.startPreview();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        if (mCamera != null) {
+            // Call stopPreview() to stop updating the preview surface.
+            mCamera.stopPreview();
+        }
+    }
+
+    public SurfaceHolder getSurfaceHolder() {
+        return mSurfaceHolder;
+    }
+
+    public void setSupportedPreviewSizes(List<Camera.Size> supportedPreviewSizes) {
+        this.mSupportedPreviewSizes = supportedPreviewSizes;
+    }
+
+    public void setCamera(Camera camera) {
+        if (mCamera == camera) { return; }
+
+        stopPreviewAndFreeCamera();
+
+        mCamera = camera;
+
+        if (mCamera != null) {
+            List<Camera.Size> localSizes = mCamera.getParameters().getSupportedPreviewSizes();
+            this.setSupportedPreviewSizes(localSizes);
+            requestLayout();
+
+            try {
+                mCamera.setPreviewDisplay(this.getSurfaceHolder());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            // Important: Call startPreview() to start updating the preview
+            // surface. Preview must be started before you can take a picture.
+            mCamera.startPreview();
+        }
+    }
+
+    private void stopPreviewAndFreeCamera() {
+
+        if (mCamera != null) {
+            // Call stopPreview() to stop updating the preview surface.
+            mCamera.stopPreview();
+
+            // Important: Call release() to release the camera for use by other
+            // applications. Applications should release the camera immediately
+            // during onPause() and re-open() it during onResume()).
+            mCamera.release();
+
+            mCamera = null;
+        }
+    }
+}
