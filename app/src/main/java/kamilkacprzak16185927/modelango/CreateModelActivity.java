0d4364fb9f94950eae9f74d517a4eb58660befd9
Kamil Kacprzak <kamilkacprzak2@gmail.com>
2018-07-02 03:25:41 +0200

    Camera added

--- app/src/main/java/kamilkacprzak16185927/modelango/CreateModelActivity.java
+++ app/src/main/java/kamilkacprzak16185927/modelango/CreateModelActivity.java
@@ -1,13 +1,83 @@
 package kamilkacprzak16185927.modelango;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 public class CreateModelActivity extends AppCompatActivity {
+    private Camera mCamera;
+    private Handler mCustomHandler = new Handler();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_model);
     }
+
+    private class CameraOpenTask extends AsyncTask<Void, Void, Camera> {
+
+        @Override
+        protected void onPreExecute() {
+        }
+
+        @Override
+        protected Camera doInBackground(Void... params) {
+            if (ContextCompat.checkSelfPermission(CreateModelActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                return safeCameraOpen(0);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Camera camera) {
+            mCamera = camera;
+            if(mCamera != null){
+
+                //Set this camera instance in camera preview
+            }
+
+
+        }
+    }
+
+    private Camera safeCameraOpen(int id) {
+
+        Camera camera;
+
+        try {
+            releaseCameraAndPreview();
+            camera = Camera.open(id);
+            if (camera != null) {
+                return camera;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private void releaseCameraAndPreview() {
+        if (mCamera != null) {
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
+//    private class Preview extends ViewGroup implements SurfaceHolder.Callback{
+//
+//        Preview(Context context){
+//            super(context);
+//        }
+//    }
 }
--- app/src/main/java/kamilkacprzak16185927/modelango/GalleryActivity.java
+++ app/src/main/java/kamilkacprzak16185927/modelango/GalleryActivity.java
@@ -1,5 +1,7 @@
 package kamilkacprzak16185927.modelango;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,12 @@ public class GalleryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp(){
+        finish();
+        return true;
     }
 }
