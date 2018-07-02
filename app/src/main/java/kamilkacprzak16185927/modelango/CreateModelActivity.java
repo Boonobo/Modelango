66338bb809d81f701456874eeb206c71ec169faa
Kamil Kacprzak <kamilkacprzak2@gmail.com>
2018-07-02 20:22:01 +0200

    Tango code implemented- ofc not working

--- app/src/main/java/kamilkacprzak16185927/modelango/CreateModelActivity.java
+++ app/src/main/java/kamilkacprzak16185927/modelango/CreateModelActivity.java
@@ -3,20 +3,48 @@ package kamilkacprzak16185927.modelango;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.hardware.display.DisplayManager;
 import android.os.AsyncTask;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.atap.tango.ux.TangoUx;
+import com.google.atap.tango.ux.UxExceptionEvent;
+import com.google.atap.tango.ux.UxExceptionEventListener;
 import com.google.atap.tangoservice.Tango;
 import com.google.atap.tangoservice.TangoConfig;
+import com.google.atap.tangoservice.TangoCoordinateFramePair;
 import com.google.atap.tangoservice.TangoErrorException;
+import com.google.atap.tangoservice.TangoEvent;
+import com.google.atap.tangoservice.TangoInvalidException;
+import com.google.atap.tangoservice.TangoOutOfDateException;
+import com.google.atap.tangoservice.TangoPointCloudData;
+import com.google.atap.tangoservice.TangoPoseData;
+import com.projecttango.tangosupport.TangoPointCloudManager;
+import com.projecttango.tangosupport.TangoSupport;
+
+import java.nio.FloatBuffer;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 
 public class CreateModelActivity extends AppCompatActivity {
+    private static final String TAG = CreateModelActivity.class.getSimpleName();
+
+    private static final String UX_EXCEPTION_EVENT_DETECTED = "Exception Detected: ";
+    private static final String UX_EXCEPTION_EVENT_RESOLVED = "Exception Resolved: ";
+
+    private static final int SECS_TO_MILLISECS = 1000;
+    private static final DecimalFormat FORMAT_THREE_DECIMAL = new DecimalFormat("0.000");
+    private static final double UPDATE_INTERVAL_MS = 100.0;
+
     private Camera mCamera;
     private Preview mPreview;
 
@@ -25,21 +53,31 @@ public class CreateModelActivity extends AppCompatActivity {
 
     private Tango mTango;
     private TangoConfig mTangoConfig;
-    private boolean mIsTangoServiceConnected;
+    private TangoUx mTangoUx;
 
+    private TangoPointCloudManager mPointCloudManager;
 
+    private double mPointCloudPreviousTimeStamp;
+
+    private boolean mIsTangoServiceConnected = false;
+
+    private double mPointCloudTimeToNextUpdate = UPDATE_INTERVAL_MS;
+
+    private int mDisplayRotation = 0;
+
+    private TextView mAverageZTextView;
+    private TextView mPointCountTextView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_model);
-        mPreview = new Preview(this.getApplicationContext(),mCamera);
 
-        mTango = new Tango(this.getApplicationContext());
-        mTangoConfig = new TangoConfig();
-        mTangoConfig = mTango.getConfig(TangoConfig.CONFIG_TYPE_CURRENT);
-        mTangoConfig.putBoolean(TangoConfig.KEY_BOOLEAN_DEPTH, true);
+        mPointCloudManager = new TangoPointCloudManager();
+        mTangoUx = setupTangoUxAndLayout();
+
+        mPreview = new Preview(this.getApplicationContext(),mCamera);
 
         mCaptureButton = (Button) findViewById(R.id.button_capture);
         mCaptureButton.setOnClickListener(mCreateView);
@@ -49,6 +87,7 @@ public class CreateModelActivity extends AppCompatActivity {
             public void onClick(View v) {
                 switch(v.getId()){
                     case R.id.button_capture:
+                        bindTangoService();
                         break;
                     default:
                         break;
@@ -56,9 +95,140 @@ public class CreateModelActivity extends AppCompatActivity {
             }
         };
 
-        mIsTangoServiceConnected = false;
+        DisplayManager displayManager = (DisplayManager) getSystemService(DISPLAY_SERVICE);
+        if (displayManager != null) {
+            displayManager.registerDisplayListener(new DisplayManager.DisplayListener() {
+                @Override
+                public void onDisplayAdded(int displayId) {
+                }
+
+                @Override
+                public void onDisplayChanged(int displayId) {
+                    synchronized (this) {
+                        setDisplayRotation();
+                    }
+                }
+
+                @Override
+                public void onDisplayRemoved(int displayId) {
+                }
+            }, null);
+        }
 
         new CameraOpenTask().execute();
+    }
+
+    private void createTangoData() {
+        mTango = new Tango(this.getApplicationContext(), new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+        mTangoConfig = new TangoConfig();
+        mTangoConfig = mTango.getConfig(TangoConfig.CONFIG_TYPE_CURRENT);
+        mTangoConfig.putBoolean(TangoConfig.KEY_BOOLEAN_DEPTH, true);
+    }
+
+    private void bindTangoService() {
+        // Initialize Tango Service as a normal Android Service. Since we call mTango.disconnect()
+        // in onPause, this will unbind Tango Service, so every time onResume gets called we
+        // should create a new Tango object.
+        mTango = new Tango(CreateModelActivity.this, new Runnable() {
+            // Pass in a Runnable to be called from UI thread when Tango is ready; this Runnable
+            // will be running on a new thread.
+            // When Tango is ready, we can call Tango functions safely here only when there are no
+            // UI thread changes involved.
+            @Override
+            public void run() {
+                // Synchronize against disconnecting while the service is being used in the OpenGL
+                // thread or in the UI thread.
+                synchronized (CreateModelActivity.this) {
+                    try {
+                        mTangoConfig = setupTangoConfig(mTango);
+                        mTango.connect(mTangoConfig);
+                        startupTango();
+                        TangoSupport.initialize(mTango);
+                        mIsTangoServiceConnected = true;
+                        setDisplayRotation();
+                    } catch (TangoOutOfDateException e) {
+                        Log.e(TAG, getString(R.string.exception_out_of_date), e);
+                    } catch (TangoErrorException e) {
+                        Log.e(TAG, getString(R.string.exception_tango_error), e);
+                        showsToastAndFinishOnUiThread(R.string.exception_tango_error);
+                    } catch (TangoInvalidException e) {
+                        Log.e(TAG, getString(R.string.exception_tango_invalid), e);
+                        showsToastAndFinishOnUiThread(R.string.exception_tango_invalid);
+                    }
+                }
+            }
+        });
+    }
+
+    /**
+     * Sets up the Tango configuration object. Make sure mTango object is initialized before
+     * making this call.
+     */
+    private TangoConfig setupTangoConfig(Tango tango) {
+        // Use the default configuration plus add depth sensing.
+        TangoConfig config = tango.getConfig(TangoConfig.CONFIG_TYPE_DEFAULT);
+        config.putBoolean(TangoConfig.KEY_BOOLEAN_DEPTH, true);
+        config.putInt(TangoConfig.KEY_INT_DEPTH_MODE, TangoConfig.TANGO_DEPTH_MODE_POINT_CLOUD);
+        return config;
+    }
+
+    private void startupTango() {
+        ArrayList<TangoCoordinateFramePair> framePairs = new ArrayList<TangoCoordinateFramePair>();
+
+        framePairs.add(new TangoCoordinateFramePair(TangoPoseData.COORDINATE_FRAME_START_OF_SERVICE,
+                TangoPoseData.COORDINATE_FRAME_DEVICE));
+
+        mTango.connectListener(framePairs, new Tango.TangoUpdateCallback() {
+            @Override
+            public void onPoseAvailable(TangoPoseData pose) {
+                // Passing in the pose data to UX library produce exceptions.
+                if (mTangoUx != null) {
+                    mTangoUx.updatePoseStatus(pose.statusCode);
+                }
+            }
+
+            @Override
+            public void onPointCloudAvailable(TangoPointCloudData pointCloud) {
+                if (mTangoUx != null) {
+                    mTangoUx.updatePointCloud(pointCloud);
+                }
+                mPointCloudManager.updatePointCloud(pointCloud);
+
+                final double currentTimeStamp = pointCloud.timestamp;
+                final double pointCloudFrameDelta =
+                        (currentTimeStamp - mPointCloudPreviousTimeStamp) * SECS_TO_MILLISECS;
+                mPointCloudPreviousTimeStamp = currentTimeStamp;
+                final double averageDepth = getAveragedDepth(pointCloud.points,
+                        pointCloud.numPoints);
+
+                mPointCloudTimeToNextUpdate -= pointCloudFrameDelta;
+
+                if (mPointCloudTimeToNextUpdate < 0.0) {
+                    mPointCloudTimeToNextUpdate = UPDATE_INTERVAL_MS;
+                    final String pointCountString = Integer.toString(pointCloud.numPoints);
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mPointCountTextView.setText(pointCountString);
+                            mAverageZTextView.setText(FORMAT_THREE_DECIMAL.format(averageDepth));
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onTangoEvent(TangoEvent event) {
+                if (mTangoUx != null) {
+                    mTangoUx.updateTangoEvent(event);
+                }
+            }
+        });
     }
 
     private class CameraOpenTask extends AsyncTask<Void, Void, Camera> {
@@ -116,28 +286,109 @@ public class CreateModelActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onPause(){
-        super.onPause();
-        if(mTango != null){
-            try{
-                mTango.disconnect();
-                mIsTangoServiceConnected = false;
-            }catch(TangoErrorException e){
-                Log.i("Tango error","Tango didn't disconnect properly");
-            }
-        }
+    protected void onStart(){
+        super.onStart();
 
+        mTangoUx.start(new TangoUx.StartParams());
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        if (!mIsTangoServiceConnected) {
-            startActivityForResult(
-                    Tango.getRequestPermissionIntent(Tango.PERMISSIONTYPE_MOTION_TRACKING),
-                    Tango.TANGO_INTENT_ACTIVITYCODE);
+    protected void onStop() {
+        super.onStop();
+
+        // Synchronize against disconnecting while the service is being used in the OpenGL
+        // thread or in the UI thread.
+        // NOTE: DO NOT lock against this same object in the Tango callback thread.
+        // Tango.disconnect will block here until all Tango callback calls are finished.
+        // If you lock against this object in a Tango callback thread it will cause a deadlock.
+        synchronized (this) {
+            try {
+                mTangoUx.stop();
+                if(mTango != null) mTango.disconnect();
+                mIsTangoServiceConnected = false;
+            } catch (TangoErrorException e) {
+                Log.e(TAG, getString(R.string.exception_tango_error), e);
+            }
         }
     }
 
+    private TangoUx setupTangoUxAndLayout() {
+        TangoUx tangoUx = new TangoUx(this.getApplicationContext());
+        tangoUx.setUxExceptionEventListener(mUxExceptionListener);
+        return tangoUx;
+    }
 
+    private UxExceptionEventListener mUxExceptionListener = new UxExceptionEventListener() {
+        @Override
+        public void onUxExceptionEvent(UxExceptionEvent uxExceptionEvent) {
+            String status = uxExceptionEvent.getStatus() == UxExceptionEvent.STATUS_DETECTED ?
+                    UX_EXCEPTION_EVENT_DETECTED : UX_EXCEPTION_EVENT_RESOLVED;
+
+            if (uxExceptionEvent.getType() == UxExceptionEvent.TYPE_LYING_ON_SURFACE) {
+                Log.i(TAG, status + "Device lying on surface");
+            }
+            if (uxExceptionEvent.getType() == UxExceptionEvent.TYPE_FEW_DEPTH_POINTS) {
+                Log.i(TAG, status + "Too few depth points");
+            }
+            if (uxExceptionEvent.getType() == UxExceptionEvent.TYPE_FEW_FEATURES) {
+                Log.i(TAG, status + "Too few features");
+            }
+            if (uxExceptionEvent.getType() == UxExceptionEvent.TYPE_MOTION_TRACK_INVALID) {
+                Log.i(TAG, status + "Invalid poses in MotionTracking");
+            }
+            if (uxExceptionEvent.getType() == UxExceptionEvent.TYPE_MOVING_TOO_FAST) {
+                Log.i(TAG, status + "Moving too fast");
+            }
+            if (uxExceptionEvent.getType() == UxExceptionEvent.TYPE_FISHEYE_CAMERA_OVER_EXPOSED) {
+                Log.i(TAG, status + "Fisheye Camera Over Exposed");
+            }
+            if (uxExceptionEvent.getType() == UxExceptionEvent.TYPE_FISHEYE_CAMERA_UNDER_EXPOSED) {
+                Log.i(TAG, status + "Fisheye Camera Under Exposed");
+            }
+        }
+    };
+
+    /**
+     * Calculates the average depth from a point cloud buffer.
+     *
+     * @param pointCloudBuffer
+     * @param numPoints
+     * @return Average depth.
+     */
+    private float getAveragedDepth(FloatBuffer pointCloudBuffer, int numPoints) {
+        float totalZ = 0;
+        float averageZ = 0;
+        if (numPoints != 0) {
+            int numFloats = 4 * numPoints;
+            for (int i = 2; i < numFloats; i = i + 4) {
+                totalZ = totalZ + pointCloudBuffer.get(i);
+            }
+            averageZ = totalZ / numPoints;
+        }
+        return averageZ;
+    }
+
+    /**
+     * Query the display's rotation.
+     */
+    private void setDisplayRotation() {
+        Display display = getWindowManager().getDefaultDisplay();
+        mDisplayRotation = display.getRotation();
+    }
+
+    /**
+     * Display toast on UI thread.
+     *
+     * @param resId The resource id of the string resource to use. Can be formatted text.
+     */
+    private void showsToastAndFinishOnUiThread(final int resId) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(CreateModelActivity.this,
+                        getString(resId), Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+    }
 }
